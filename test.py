9761f53ead17bf9b40644494b8e7e7e7455b9749
Sejin Chun <chunsejin@gmail.com>
2025-04-08 13:24:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,7 @@
-def messy_calc(a, b, c):
-    res = 0
-    if a > 0:
-        res = a * b
-    elif a < 0:
-        res = a + b
-    else:
-        res = b - a
-
-    if c > 10:
-        res = res / 2
-    elif c < 5:
-        res = res * 3
-    else:
-        res = res + c
-
-    return res
-
-# Messy usage examples
-print(messy_calc(5, 2, 12))
-print(messy_calc(-3, 4, 3))
-print(messy_calc(0, 7, 8))
-print(messy_calc(2,3,7))
-print(messy_calc(-4,5,11))
-print(messy_calc(1,2,3))
+kubernetes
+GraphQL
+TensorFlow
+Numpy
+blockchain
+OAuth
+Github
